7ba21ed70ef18f391fd522bbe757480844072357
jaku98 <mjakubczyk98@gmail.com>
2021-12-02 18:00:20 +0100

    1.0.0

--- radar_sim.py
+++ radar_sim.py
@@ -1,28 +1,43 @@
 # Model programowy systemu zobrazowania sytuacji powietrznej w radarze pokładowym
-
 import pygame
 
-pygame.init()
 # Definiowanie okna gry
-win = pygame.display.set_mode((1280, 1024))
-# Wyświetlenie okna gry
+pygame.init()
+screen = pygame.display.set_mode((1280, 1024))
 pygame.display.set_caption("RADAR Air to Air")
-# Dodawanie tła
 
+# Czarne tło pod belką
+background_x = 0
+background_y = 570
+background_width = 1280
+background_height = 454
+background__color = (0, 0, 0)
 
+def black(x, y, w, h):
+    pygame.draw.rect(screen, background__color, (x, y, w, h))
 
-x = 0
-y = 40
-szerokosc = 20
-wysokosc = 20
-krok = 20
+# Dodawanie kokpitu
+cockpit_pic = pygame.image.load("pic/cockpit_new.bmp")
+cockpit_x = 0
+cockpit_y = 0
 
+def cockpit(x, y):
+    screen.blit(cockpit_pic,(x, y))
+
+# Sky
+sky_pic = pygame.image.load("pic/sky.jpg") 
+sky_x = -255
+sky_y = -420
+sky_step = 5
+
+def sky(x,y):
+    screen.blit(sky_pic, (x, y))
 
 run = True
 # Pętla główna
 while run:
     # opóźnienie w grze
-    pygame.time.delay(20)
+    pygame.time.delay(50)
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -32,17 +47,17 @@ while run:
     keys = pygame.key.get_pressed()
     # warunki do zmiany pozycji obiektu
     if keys[pygame.K_LEFT]:
-        x -= krok
+        sky_x -= sky_step
     if keys[pygame.K_RIGHT]:
-        x += krok
+        sky_x += sky_step
     if keys[pygame.K_UP]:
-        y -= krok
+        sky_y -= sky_step
     if keys[pygame.K_DOWN] :
-        y += krok
+        sky_y += sky_step
 
-    pygame.draw.rect(win, (0, 255, 0), (x, y, szerokosc, wysokosc))
-
-    cockpit = pygame.image.load("pic/cockpit_new.bmp")
-    win.blit(cockpit,(0, 0))
+    
+    sky(sky_x, sky_y)
+    black(background_x, background_y, background_width, background_height)
+    cockpit(cockpit_x, cockpit_y)
 
     pygame.display.update()
