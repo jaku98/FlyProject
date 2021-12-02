188a2a9aebdf6867c0c231ce710178eedb296ab6
jaku98 <mjakubczyk98@gmail.com>
2021-12-02 16:33:59 +0100

    1.0.0

--- radar_sim.py
+++ radar_sim.py
@@ -1,1 +1,9 @@
 # Model programowy systemu zobrazowania sytuacji powietrznej w radarze pokładowym
+
+import pygame
+
+pygame.init()
+# Definiowanie okna gry
+win = pygame.display.set_mode((1280, 1024))
+# Wyświetlenie okna gry
+pygame.display.set_caption("RADAR Air to Air")
