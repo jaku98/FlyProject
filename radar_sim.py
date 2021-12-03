1ada8247d062da7868f680a9c6a623fea8515ee1
jaku98 <mjakubczyk98@gmail.com>
2021-12-03 16:14:23 +0100

    1.0.1

--- radar_sim.py
+++ radar_sim.py
@@ -26,23 +26,30 @@ def cockpit(x, y):
 
 # Sky
 sky_pic = pygame.image.load("pic/sky.jpg") 
+#h = (sky_pic.get_height())
+#w = (sky_pic.get_width())
+#sky_pic = pygame.transform.scale(sky_pic, h*2, w*2)
 sky_x = -255 # Wyśrodkowanie horyzontu
 sky_y = -420
 sky_step = 5
 sky_angle = 0
 
-def sky(x,y):
+def sky(x, y, pic, a):
+    #screen.blit(sky_pic, (x, y))
+    orig_rect = pic.get_rect()
+    rot_image = pygame.transform.rotate(pic, a)
+    rot_rect = orig_rect.copy()
+    rot_rect.center = rot_image.get_rect().center
+    rot_image = rot_image.subsurface(rot_rect).copy()
+    sky_pic = rot_image
     screen.blit(sky_pic, (x, y))
-def sky_a(a, x, y):
-    sky_pic_copy = pygame.transform.rotate(sky_pic, a)
-    screen.blit(sky_pic, (x - int(sky_pic.get_width() / 2), y - int(sky_pic.get_height() / 2)))
-
+    return sky_pic
 
 run = True
 # Pętla główna
 while run:
     # opóźnienie w grze
-    pygame.time.delay(50)
+    pygame.time.delay(20)
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -51,20 +58,20 @@ while run:
     # obsługa zdarzeń 
     keys = pygame.key.get_pressed()
     # warunki do zmiany pozycji obiektu
-    if keys[pygame.K_LEFT]:
-        sky_x -= sky_step
     if keys[pygame.K_RIGHT]:
+        sky_x -= sky_step
+    if keys[pygame.K_LEFT]:
         sky_x += sky_step
     if keys[pygame.K_UP]:
         sky_y -= sky_step
     if keys[pygame.K_DOWN]:
         sky_y += sky_step
-    if keys[pygame.K_a]:
-        sky_y += sky_step
     if keys[pygame.K_d]:
-        sky_y += sky_step
+        sky_angle += sky_step/5
+    if keys[pygame.K_a]:
+        sky_angle -= sky_step/5
     
-    sky(sky_x, sky_y)
+    sky(sky_x, sky_y, sky_pic, sky_angle)
     black(background_x, background_y, background_width, background_height)
     cockpit(cockpit_x, cockpit_y)
 
