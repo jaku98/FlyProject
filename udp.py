f035e1cdcb9b87f64a64014f8a0314b52660d87b
jaku98 <mjakubczyk98@gmail.com>
2022-01-20 14:40:24 +0100

    1.0.0

---
+++ udp.py
@@ -0,0 +1,24 @@
+import socket, time, sys
+import struct
+import codecs
+from numpy import byte
+
+UDP_IP = "" #"172.23.183.43"
+UDP_PORT = 8000
+BUFFER_SIZE = 512
+
+sock = socket.socket(socket.AF_INET, # Internet
+                     socket.SOCK_DGRAM) # UDP
+sock.bind((UDP_IP, UDP_PORT))
+data = []
+try:
+    run = True
+    while run:
+        data, addr = sock.recvfrom(BUFFER_SIZE) # buffer size
+        data = struct.unpack("20f3c", data)
+        print("Game Time: ", data[0])
+
+except KeyboardInterrupt:
+    print('exit')
+    pass #Ctrl+C to terminate
+
