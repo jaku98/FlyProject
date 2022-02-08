0b0daa66b8d69c0d8098a42d55be358fc1c4bad7
jaku98 <mjakubczyk98@gmail.com>
2022-02-08 18:19:24 +0100

    1.3

--- radar_sim.py
+++ radar_sim.py
@@ -1,35 +1,46 @@
 # Model programowy systemu zobrazowania sytuacji powietrznej w radarze pokładowym
+from turtle import screensize
 import pygame, sys
 import socket, struct
 import numpy as np
 
-UDP_IP = ""
-UDP_PORT = 8000
-BUFFER_SIZE = 512
-sock = socket.socket(socket.AF_INET, # Internet
-                socket.SOCK_DGRAM) # UDP
-sock.bind((UDP_IP, UDP_PORT))
 
-def receive():
-    data, addr = sock.recvfrom(BUFFER_SIZE) # buffer size
-    data = struct.unpack("20f3c", data)
-    
-# Definiowanie okna 
-pygame.init()
-screen = pygame.display.set_mode((600, 600))
-pygame.display.set_caption("RADAR Air to Air")
-cockpit_pic = pygame.image.load("pic/fcr_pic.png")
-cockpit_x = 0
-cockpit_y = 0
+class UDPConnection:
 
-def cockpit(x, y):
-    screen.blit(cockpit_pic,(x, y))
+    def __init__(self, udp_ip, udp_port):
+        self.udp_ip = udp_ip #""
+        self.udp_port = udp_port #8000
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-# Czerwnone kółko po kliknięciu
-def circle(pos):
-    circle_c = (255,0,0)
-    circle_r = 10
-    pygame.draw.circle(screen, circle_c, event.pos, circle_r)
+    def connect(self):
+        self.sock.bind((self.udp_ip, self.udp_port)) 
+        print("UDP connection with UE4: OK!")
+
+    def receive(self): 
+        self.data, self.addr = self.sock.recvfrom(512) # buffer size
+        self.data = struct.unpack("20f3c", self.data) # unpack data
+        return self.data
+
+
+class Cockpit:
+
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((600, 600))
+        pygame.display.set_caption("RADAR Air to Air")
+        self.cockpit_pic = pygame.image.load("pic/fcr_pic.png")
+
+    def cockpit(self):
+        self._x = 0
+        self._y = 0
+        self.screen.blit(self.cockpit_pic,(self._x, self._y))
+
+    # Czerwnone kółko po kliknięciu
+    def circle(self, pos):
+        self.pos = pos
+        self.circle_c = (255,0,0) # color
+        self.circle_r = 10 # size
+        pygame.draw.circle(self.screen, self.circle_c, event.pos, self.circle_r)
 
 # Macierz przycisków FCR
 FCR_x_left = 34
@@ -40,15 +51,23 @@ FCR_y_matrix = [435, 435-66, 435-2*66, 435-3*66, 435-4*66]
 FCR_x_matrix = [151, 151+73, 151+2*73, 151+3*73, 151+4*73]
 FCR_button = 20.0
 
+myUDP = UDPConnection("", 8000)
+myUDP.connect()
+pyGame = Cockpit()
+
 run = True
 # Pętla główna
 while run:
-    data = []
     # opóźnienie w grze
-    pygame.time.delay(20)
+    pygame.time.delay(1)
+
+    # receive decoded message
+    message = myUDP.receive()
+    print(message)
+    
     # Inicjalizacja grafiki
-    cockpit(cockpit_x, cockpit_y)
-    receive()
+    pyGame.cockpit()
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
@@ -64,54 +83,55 @@ while run:
     if event.type == pygame.MOUSEBUTTONDOWN:
         mouse_pos = pygame.mouse.get_pos()
         print('x', mouse_pos[0], 'y', mouse_pos[1])
+        click = pyGame.circle(mouse_pos)
 
         # Lewa kolumna button FCR
         if  ((mouse_pos[0]-FCR_button)<FCR_x_left<(mouse_pos[0]+FCR_button)):
             if ((mouse_pos[1]-FCR_button)<FCR_y_matrix[0]<(mouse_pos[1]+FCR_button)):
-                circle(mouse_pos)
+                click
             if ((mouse_pos[1]-FCR_button)<FCR_y_matrix[1]<(mouse_pos[1]+FCR_button)):
-                circle(mouse_pos)
+                click
             if ((mouse_pos[1]-FCR_button)<FCR_y_matrix[2]<(mouse_pos[1]+FCR_button)):
-                circle(mouse_pos)
+                click
             if ((mouse_pos[1]-FCR_button)<FCR_y_matrix[3]<(mouse_pos[1]+FCR_button)):
-                circle(mouse_pos)
+                click
             if ((mouse_pos[1]-FCR_button)<FCR_y_matrix[4]<(mouse_pos[1]+FCR_button)):
-                circle(mouse_pos)
-        # Prawa kolumna button FCR
-        if  ((mouse_pos[0]-FCR_button)<FCR_x_right<(mouse_pos[0]+FCR_button)):
-            if ((mouse_pos[1]-FCR_button)<FCR_y_matrix[0]<(mouse_pos[1]+FCR_button)):
-                circle(mouse_pos)
-            if ((mouse_pos[1]-FCR_button)<FCR_y_matrix[1]<(mouse_pos[1]+FCR_button)):
-                circle(mouse_pos)
-            if ((mouse_pos[1]-FCR_button)<FCR_y_matrix[2]<(mouse_pos[1]+FCR_button)):
-                circle(mouse_pos)
-            if ((mouse_pos[1]-FCR_button)<FCR_y_matrix[3]<(mouse_pos[1]+FCR_button)):
-                circle(mouse_pos)
-            if ((mouse_pos[1]-FCR_button)<FCR_y_matrix[4]<(mouse_pos[1]+FCR_button)):
-                circle(mouse_pos)
-        # Dolny wiersz button FCR
-        if  ((mouse_pos[1]-FCR_button)<FCR_y_down<(mouse_pos[1]+FCR_button)):
-            if ((mouse_pos[0]-FCR_button)<FCR_x_matrix[0]<(mouse_pos[0]+FCR_button)):
-                circle(mouse_pos)
-            if ((mouse_pos[0]-FCR_button)<FCR_x_matrix[1]<(mouse_pos[0]+FCR_button)):
-                circle(mouse_pos)
-            if ((mouse_pos[0]-FCR_button)<FCR_x_matrix[2]<(mouse_pos[0]+FCR_button)):
-                circle(mouse_pos)
-            if ((mouse_pos[0]-FCR_button)<FCR_x_matrix[3]<(mouse_pos[0]+FCR_button)):
-                circle(mouse_pos)
-            if ((mouse_pos[0]-FCR_button)<FCR_x_matrix[4]<(mouse_pos[0]+FCR_button)):
-                circle(mouse_pos)
-        # Górny wiersz button FCR
-        if  ((mouse_pos[1]-FCR_button)<FCR_y_up<(mouse_pos[1]+FCR_button)):
-            if ((mouse_pos[0]-FCR_button)<FCR_x_matrix[0]<(mouse_pos[0]+FCR_button)):
-                circle(mouse_pos)
-            if ((mouse_pos[0]-FCR_button)<FCR_x_matrix[1]<(mouse_pos[0]+FCR_button)):
-                circle(mouse_pos)
-            if ((mouse_pos[0]-FCR_button)<FCR_x_matrix[2]<(mouse_pos[0]+FCR_button)):
-                circle(mouse_pos)
-            if ((mouse_pos[0]-FCR_button)<FCR_x_matrix[3]<(mouse_pos[0]+FCR_button)):
-                circle(mouse_pos)
-            if ((mouse_pos[0]-FCR_button)<FCR_x_matrix[4]<(mouse_pos[0]+FCR_button)):
-                circle(mouse_pos)
+                click
+        # # Prawa kolumna button FCR
+        # if  ((mouse_pos[0]-FCR_button)<FCR_x_right<(mouse_pos[0]+FCR_button)):
+        #     if ((mouse_pos[1]-FCR_button)<FCR_y_matrix[0]<(mouse_pos[1]+FCR_button)):
+        #         Cockpit.circle(mouse_pos)
+        #     if ((mouse_pos[1]-FCR_button)<FCR_y_matrix[1]<(mouse_pos[1]+FCR_button)):
+        #         Cockpit.circle(mouse_pos)
+        #     if ((mouse_pos[1]-FCR_button)<FCR_y_matrix[2]<(mouse_pos[1]+FCR_button)):
+        #         Cockpit.circle(mouse_pos)
+        #     if ((mouse_pos[1]-FCR_button)<FCR_y_matrix[3]<(mouse_pos[1]+FCR_button)):
+        #         Cockpit.circle(mouse_pos)
+        #     if ((mouse_pos[1]-FCR_button)<FCR_y_matrix[4]<(mouse_pos[1]+FCR_button)):
+        #         Cockpit.circle(mouse_pos)
+        # # Dolny wiersz button FCR
+        # if  ((mouse_pos[1]-FCR_button)<FCR_y_down<(mouse_pos[1]+FCR_button)):
+        #     if ((mouse_pos[0]-FCR_button)<FCR_x_matrix[0]<(mouse_pos[0]+FCR_button)):
+        #         Cockpit.circle(mouse_pos)
+        #     if ((mouse_pos[0]-FCR_button)<FCR_x_matrix[1]<(mouse_pos[0]+FCR_button)):
+        #         Cockpit.circle(mouse_pos)
+        #     if ((mouse_pos[0]-FCR_button)<FCR_x_matrix[2]<(mouse_pos[0]+FCR_button)):
+        #         Cockpit.circle(mouse_pos)
+        #     if ((mouse_pos[0]-FCR_button)<FCR_x_matrix[3]<(mouse_pos[0]+FCR_button)):
+        #         Cockpit.circle(mouse_pos)
+        #     if ((mouse_pos[0]-FCR_button)<FCR_x_matrix[4]<(mouse_pos[0]+FCR_button)):
+        #         Cockpit.circle(mouse_pos)
+        # # Górny wiersz button FCR
+        # if  ((mouse_pos[1]-FCR_button)<FCR_y_up<(mouse_pos[1]+FCR_button)):
+        #     if ((mouse_pos[0]-FCR_button)<FCR_x_matrix[0]<(mouse_pos[0]+FCR_button)):
+        #         Cockpit.circle(mouse_pos)
+        #     if ((mouse_pos[0]-FCR_button)<FCR_x_matrix[1]<(mouse_pos[0]+FCR_button)):
+        #         Cockpit.circle(mouse_pos)
+        #     if ((mouse_pos[0]-FCR_button)<FCR_x_matrix[2]<(mouse_pos[0]+FCR_button)):
+        #         Cockpit.circle(mouse_pos)
+        #     if ((mouse_pos[0]-FCR_button)<FCR_x_matrix[3]<(mouse_pos[0]+FCR_button)):
+        #         Cockpit.circle(mouse_pos)
+        #     if ((mouse_pos[0]-FCR_button)<FCR_x_matrix[4]<(mouse_pos[0]+FCR_button)):
+        #         Cockpit.circle(mouse_pos)
 
     pygame.display.update()
