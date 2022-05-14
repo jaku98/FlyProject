3a26dabb3b6122165a175fd74b91af6f71a1a9f2
jaku98 <mjakubczyk98@gmail.com>
2022-05-15 00:29:09 +0200

    2.0

--- RadarSoft.py
+++ RadarSoft.py
@@ -1,8 +1,8 @@
 # Title [pol]: Model programowy systemu zobrazowania sytuacji powietrznej w radarze pokładowym
 # Title [eng]: Radar software model of airborne situational awareness system
 # Author: JAKUBCZYK MARCIN, Military University of Technology in Warsaw, 2022
-# Description: The program works with a simulation created in the Unreal Engine 4.
-#              Information display system in FCR format.
+# Description: The program works with a simulation created in the Unreal Engine 4
+#              Information display system in FCR format with basic operation
 import pygame as pg
 from pygame.locals import *
 import socket, struct, select, sys, gc, os
