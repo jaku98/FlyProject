398b30b2a2e92acea96f8839310c7b4f7813c11a
jaku98 <mjakubczyk98@gmail.com>
2022-05-15 22:22:23 +0200

    2.0

--- AboutSave.py
+++ AboutSave.py
@@ -6,7 +6,7 @@ f2 = open(filepath2, "w")
 
 lines = ['''[POL] Aplikacja modeluje system zobrazowania sytuacji powietrznej instalowany w samolotach mysliwskich. Zobrazowanie informacji przedstawione zostalo na wyswietlaczu MDF w formacie FCR \n
 [ENG] The application models the airborne situation display system installed in fighter aircrafts. 
-The information display was presented on the MDF display in the FCR format \n
+The information display was presented on the MDF display in the FCR format. \n
 KEYBOARD:
 Simulation: W/S - Acceleration, A/D - Yaw, Q/E - Roll, UP/DOWN - Pitch, P - Pause, MOUSE
 MFD: T/G - Elevation tilt, F/H - Azimuth yaw, ARROWS - Acquisition cursor, Space - STT, MOUSE''']
