e101167fba6f3e7a5845437fc4f8345ab44fceac
jaku98 <mjakubczyk98@gmail.com>
2022-05-21 21:58:11 +0200

    2.0

--- AboutSave.py
+++ AboutSave.py
@@ -1,10 +1,10 @@
 filepath1 = 'D:\\Unreal Projects\\FlyProject2\\about.txt'
-filepath2 = 'D:\\Unreal Projects\\AplikacjaGotowa\\WindowsNoEditor\\FlyProject\\about.txt'
+filepath2 = 'D:\\Unreal Projects\\AplikacjaGotowa\\Radar_software\\FlyProject\\about.txt'
 
 f1 = open(filepath1, "w")
 f2 = open(filepath2, "w")
 
-lines = ['''[POL] Aplikacja modeluje system zobrazowania sytuacji powietrznej instalowany w samolotach mysliwskich. Zobrazowanie informacji przedstawione zostalo na wyswietlaczu MDF w formacie FCR. \n
+lines = ['''[POL] Aplikacja modeluje system zobrazowania sytuacji powietrznej instalowany w samolotach wielozadaniowych. Zobrazowanie informacji przedstawione zostalo na wyswietlaczu MDF w formacie FCR. \n
 [ENG] The application models the airborne situation display system installed in fighter aircrafts. 
 The information display was presented on the MDF display in the FCR format. \n
 KEYBOARD:
